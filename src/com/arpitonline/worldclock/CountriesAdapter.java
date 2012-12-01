2c6ec8acffd51de8e21f1c6cd6dd6ed7f6325a38
Arpit Mathur <mathur.arpit@gmail.com>
2012-12-01 14:58:15 -0500

    UI changes

--- src/com/arpitonline/worldclock/CountriesAdapter.java
+++ src/com/arpitonline/worldclock/CountriesAdapter.java
@@ -11,7 +11,6 @@ import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import com.arpitonline.worldclock.models.LocationVO;
-import com.arpitonline.worldclock.R;
 
 public class CountriesAdapter extends ArrayAdapter<LocationVO> {
 
